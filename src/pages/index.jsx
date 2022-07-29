359674f7865a28c6f26427d3ef09a933fb422af2
yusufneeson <yusufneeson@gmail.com>
2022-07-29 16:02:14 +0700

    login screen: wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,14 +10,57 @@ export default function Home() {
 							Sign in to your account
 						</h2>
 					</div>
-					<form className="mt-8 space-y-6">
-						<div className="rounded-md shadow-sm -space-y-px p-5">
+					<div className="rounded-md shadow-sm py-6 px-5 border border-gray-100">
+						<div className="grid gap-6">
+							<button className="h-12 px-6 border border-blue-50 rounded-lg bg-blue-50">
+								<div className="flex items-center space-x-4 justify-center">
+									<span className="block w-max font-medium tracking-wide text-sm text-blue-700">
+										with Google
+									</span>
+								</div>
+							</button>
+						</div>
+
+						<div className="mt-12 border-t">
+							<span className="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white">
+								OR
+							</span>
+						</div>
+
+						<form className="mt-8 space-y-6">
 							<div>
-								<label htmlFor="email">Email</label>
+								<label
+									htmlFor="email"
+									className="text-gray-600"
+								>
+									Email
+								</label>
 								<Input id="email" type="email" />
 							</div>
-						</div>
-					</form>
+							<div className="mt-4">
+								<label
+									htmlFor="password"
+									className="text-gray-500"
+								>
+									Password
+								</label>
+								<Input id="password" type="password" />
+							</div>
+							<div className="mt-8">
+								<button className="w-full rounded-lg px-6 py-2 bg-sky-500 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-700">
+									<span className="text-white font-medium text-lg">
+										Login
+									</span>
+								</button>
+								<a
+									href="#"
+									className="text-sm tracking-wide mt-3 block text-sky-600"
+								>
+									Create new account
+								</a>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Rubik", "sans-serif"],
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/forms")],
 };
